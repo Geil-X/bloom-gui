b37a39a03cb85e6cc6f8efd1e8314e218105b6d4
Thomas Waters <twaters117@gmail.com>
2022-03-23 13:27:58 -0400

    Styles

--- Gui/Program.fs
+++ Gui/Program.fs
@@ -1,8 +1,13 @@
 namespace Gui
 
+open System
 open Avalonia
 open Avalonia.Controls.ApplicationLifetimes
 open Avalonia.FuncUI
+open Avalonia.Themes.Default
+
+open AvaloniaStyles
+open Gui.Views.Styles
 
 /// This is your application you can ose the initialize method to load styles
 /// or handle Life Cycle events of your application
@@ -10,10 +15,12 @@ type App() =
     inherit Application()
 
     override this.Initialize() =
-        this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
-        this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
-        this.Styles.Load "avares://Gui/Styles.xaml"
-        
+        //        this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
+//        this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
+//        this.Styles.Load "avares://Gui/Styles.xaml"
+//        this.Styles.Add(DefaultTheme())
+        this.Styles.Add(Theme(Uri "avares://ControlCatalog/Styles"))
+
         this.Name <- Theme.program
 
     override this.OnFrameworkInitializationCompleted() =
@@ -26,7 +33,8 @@ module Program =
 
     [<EntryPoint>]
     let main (args: string []) =
-        AppBuilder.Configure<App>()
+        AppBuilder
+            .Configure<App>()
             .UsePlatformDetect()
             .UseSkia()
             .StartWithClassicDesktopLifetime(args)
